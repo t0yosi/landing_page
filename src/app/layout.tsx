3e999d841da0137630f49d746e06721366dc79a4
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-18 17:04:06 +0100

    start-up

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Startup Landing Page",
+  description: "A landing page for a startup created by Weng&Benjamin",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
