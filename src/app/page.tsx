314e27bce12a1a53ab984524baa3f4ab7b5abb30
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-22 11:10:23 +0100

    Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import { Features } from "@/sections/Features";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
+import { Testimonials } from "@/sections/Testimonials";
 
 export default function Home() {
   return (
     <>
-      <Header /> <Hero /> <LogoTicker/> <Features/>
+      <Header /> <Hero /> <LogoTicker/> <Features/> <Testimonials/>
     </>
   );
 }
