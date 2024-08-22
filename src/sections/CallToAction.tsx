1980c17a78b092a6921daafce5fc3c496a433ce8
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-22 18:43:44 +0100

    call to action animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,23 +1,89 @@
+"use client";
 import { Button } from "@/components/Button";
 import starsBg from "@/assets/stars.png";
 import gridLines from "@/assets/grid-lines.png";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { RefObject, useEffect, useRef } from "react";
+
+const useRelativeMousePosition = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if (!to.current) return;
+    const { top, left } = to.current?.getBoundingClientRect();
+    mouseX.set(event.x - left);
+    mouseY.set(event.y - top);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return [mouseX, mouseY];
+};
 
 export const CallToAction = () => {
+  const sectionRef = useRef<HTMLElement>(null);
+  const borderedDivRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-400, 300]
+  );
+
+  const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef);
+
+  const imageMask = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px, black, transparent)`;
+
   return (
-    <section className="py-20 md:py-24">
+    <section className="py-20 md:py-24" ref={sectionRef}>
       <div className="container">
-        <div
-          className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
+        <motion.div
+          ref={borderedDivRef}
+          className="border border-white/15 py-24 rounded-xl overflow-hidden relative group"
+          animate={{
+            backgroundPositionX: starsBg.width,
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 60,
+            ease: "linear",
+          }}
           style={{
+            backgroundPositionY,
             backgroundImage: `url(${starsBg.src})`,
           }}
         >
           <div
-            className="absolute inset-0 bg-orange-500 bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            className="absolute inset-0 bg-orange-500 bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700"
             style={{
               backgroundImage: `url(${gridLines.src})`,
             }}
           ></div>
+          <motion.div
+            className="absolute inset-0 bg-orange-500 bg-blend-overlay  opacity-0 group-hover:opacity-100 transition duration-700"
+            style={{
+              maskImage: imageMask,
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></motion.div>
           <div className="relative">
             <h2 className="text-4xl md:text-5xl max-w-md mx-auto tracking-tighter text-center font-medium">
               Tailored Solutions for everyone
@@ -29,7 +95,7 @@ export const CallToAction = () => {
               <Button>Contact Us</Button>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
