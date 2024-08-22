90f82258460c2d9b129fb9f17c3e324041b68cda
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-22 17:16:44 +0100

    testimonials animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,10 @@
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -41,14 +43,25 @@ export const Testimonials = () => {
         <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
           Our unique solutions have transformed our clients' business
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map((testimonial) => (
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{ translateX: "-50%" }}
+            animate={{ translateX: "0" }}
+            transition={{
+              repeat: Infinity,
+              duration: 30,
+              ease: "linear",
+            }}
+            className="flex gap-5 flex-none"
+          >
+            {[...testimonials, ...testimonials].map((testimonial) => (
               <div
                 key={testimonial.name}
                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(233,187,44,.3),black)] max-w-xs md:max-w-md flex-none"
               >
-                <div className="text-lg tracking-tight md:text-xl">{testimonial.text}</div>
+                <div className="text-lg tracking-tight md:text-xl">
+                  {testimonial.text}
+                </div>
                 <div className="flex items-center gap-3 mt-5">
                   <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(233,187,44)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/50 before:z-10 before:rounded-lg">
                     <Image
@@ -66,7 +79,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
