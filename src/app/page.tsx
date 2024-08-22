81d345b2cce74c5642fe7586c1c8d9bf4115946a
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-22 13:18:59 +0100

    Call to Action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from "@/sections/CallToAction";
 import { Features } from "@/sections/Features";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
@@ -7,7 +8,7 @@ import { Testimonials } from "@/sections/Testimonials";
 export default function Home() {
   return (
     <>
-      <Header /> <Hero /> <LogoTicker/> <Features/> <Testimonials/>
+      <Header /> <Hero /> <LogoTicker/> <Features/> <Testimonials/> <CallToAction/>
     </>
   );
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,36 @@
+import { Button } from "@/components/Button";
+import starsBg from "@/assets/stars.png";
+import gridLines from "@/assets/grid-lines.png";
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div
+          className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
+          style={{
+            backgroundImage: `url(${starsBg.src})`,
+          }}
+        >
+          <div
+            className="absolute inset-0 bg-orange-500 bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            style={{
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></div>
+          <div className="relative">
+            <h2 className="text-4xl md:text-5xl max-w-md mx-auto tracking-tighter text-center font-medium">
+              Tailored Solutions for everyone
+            </h2>
+            <p className="text-center text-lg md:text-xl max-w-sm mx-auto text-white/70 px-4 mt-5 tracking-tight">
+              Achive clear, impactful results without the complexity
+            </p>
+            <div className="flex justify-center mt-8">
+              <Button>Contact Us</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
