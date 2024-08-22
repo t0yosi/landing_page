b130a1c286493112464ceafea00715c70430bdd7
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-08-22 14:41:17 +0100

    ring 3 transition + section transition

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,44 @@
 "use client";
 import { Button } from "@/components/Button";
 import starsBg from "@/assets/stars.png";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  // useMotionValueEvent(scrollYProgress, 'change', (value) => {
+  //   console.log(value);
+  // })
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
   return (
-    <section
+    <motion.section
       className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
       style={{
         backgroundImage: `url(${starsBg.src})`,
+        backgroundPositionY,
+      }}
+      animate={{
+        backgroundPositionX: starsBg.width,
+      }}
+      transition={{
+        duration: 120,
+        repeat: Infinity,
+        ease: "linear",
       }}
     >
       <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(230,126,34,.2)_15%,rgb(247,220,111,.2)_25%,transparent_100%)]"></div>
@@ -60,10 +90,24 @@ export const Hero = () => {
       {/* End Ring 2 */}
 
       {/* Start Ring 3 */}
-      <div className="absolute size-[544px] md:size-[980px] top-1/2 left-1/2 border border-white opacity-20 rounded-full -translate-x-1/2 -translate-y-1/2 border-dashed">
+      <motion.div
+        style={{
+          translateY: "-50%",
+          translateX: "-50%",
+        }}
+        animate={{
+          rotate: "1turn",
+        }}
+        transition={{
+          duration: 90,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+        className="absolute size-[544px] md:size-[980px] top-1/2 left-1/2 border border-white opacity-20 rounded-full -translate-x-1/2 -translate-y-1/2 border-dashed"
+      >
         <div className="absolute size-2 top-1/2 left-0 bg-white rounded-full -translate-x-1/2 -translate-y-1/2"></div>
         <div className="absolute size-2 top-1/2 left-full bg-white rounded-full -translate-x-1/2 -translate-y-1/2"></div>
-      </div>
+      </motion.div>
       {/* End Ring 3 */}
 
       <div className="container relative mt-16">
@@ -78,6 +122,6 @@ export const Hero = () => {
           <Button>Book a Session</Button>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
